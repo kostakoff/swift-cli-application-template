0a770ab7e4f9f568b007a1749ccd5c2f68959bbf
kostakoff <kostakoff@local>
2025-04-13 20:18:24 -0700

    added sonar scanner

--- distribute.sh
+++ distribute.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 #echo "📦  Updating Swift packages..."
 #swift package update
 
--- sonar.sh
+++ sonar.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 #echo "🧪  Test Swift packages..."
 
 echo "🧪  Test Swift packages..."
